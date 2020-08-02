c9b19c64854b25e77a60d28657b56589e6d4a97b
connor.aitken@gmail.com <connor.aitekn@gmail.com>
2020-08-02 00:20:27 -0400

    0.12 upgrade

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
 resource "aws_vpc" "override" {
-  cidr_block           = "${var.vpc_cidr}"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
 
-  tags = "${merge(var.tags, map("Name", format("%s", var.name)))}"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = format("%s", var.name)
+    },
+  )
 }
 
 # module "ssh_keypair_aws_override" {
@@ -37,31 +42,36 @@ cat <<CONFIG >/etc/consul.d/consul-client.json.example
 }
 CONFIG
 EOF
+
 }
 
 module "network_aws" {
   # source = "github.com/hashicorp-modules/network-aws"
   source = "./modules/network/"
 
-  create            = "${var.create}"
-  name              = "${var.name}"
-  create_vpc        = "${var.create_vpc}"
-  vpc_id            = "${aws_vpc.override.id}"
-  vpc_cidr          = "${aws_vpc.override.cidr_block}"
-  vpc_cidrs_public  = "${var.vpc_cidrs_public}"
-  nat_count         = "${var.nat_count}"
-  vpc_cidrs_private = "${var.vpc_cidrs_private}"
-  release_version   = "${var.release_version}"
+  create            = var.create
+  name              = var.name
+  create_vpc        = var.create_vpc
+  vpc_id            = aws_vpc.override.id
+  vpc_cidr          = aws_vpc.override.cidr_block
+  vpc_cidrs_public  = var.vpc_cidrs_public
+  nat_count         = var.nat_count
+  vpc_cidrs_private = var.vpc_cidrs_private
+  release_version   = var.release_version
+
   # consul_version    = "${var.consul_version}"
   # vault_version     = "${var.vault_version}"
   # nomad_version     = "${var.nomad_version}"
   # os                = "${var.os}"
   # os_version        = "${var.os_version}"
-  bastion_count     = "${var.bastion_count}"
+  bastion_count = var.bastion_count
+
   # instance_profile  = "${module.consul_auto_join_instance_role_override.instance_profile_id}" # Override instance_profile
-  instance_type     = "${var.instance_type}"
-  user_data         = "${data.template_file.bastion_user_data.rendered}" # Custom user_data
+  instance_type = var.instance_type
+  user_data     = data.template_file.bastion_user_data.rendered # Custom user_data
+
   # ssh_key_name      = "${module.ssh_keypair_aws_override.name}"
   # ssh_key_override  = "true"
-  tags              = "${var.tags}"
+  tags = var.tags
 }
+
--- variables.tf
+++ variables.tf
@@ -1,17 +1,54 @@
-variable "create"            { }
-variable "name"              { }
-variable "create_vpc"        { }
-variable "rsa_bits"          { }
-variable "vpc_cidr"          { }
-variable "vpc_cidrs_public"  { type = "list" }
-variable "nat_count"         { }
-variable "vpc_cidrs_private" { type = "list" }
-variable "release_version"   { }
-variable "consul_version"    { }
-variable "vault_version"     { }
-variable "nomad_version"     { }
-variable "os"                { }
-variable "os_version"        { }
-variable "bastion_count"     { }
-variable "instance_type"     { }
-variable "tags"              { type = "map" }
+variable "create" {
+}
+
+variable "name" {
+}
+
+variable "create_vpc" {
+}
+
+variable "rsa_bits" {
+}
+
+variable "vpc_cidr" {
+}
+
+variable "vpc_cidrs_public" {
+  type = list(string)
+}
+
+variable "nat_count" {
+}
+
+variable "vpc_cidrs_private" {
+  type = list(string)
+}
+
+variable "release_version" {
+}
+
+variable "consul_version" {
+}
+
+variable "vault_version" {
+}
+
+variable "nomad_version" {
+}
+
+variable "os" {
+}
+
+variable "os_version" {
+}
+
+variable "bastion_count" {
+}
+
+variable "instance_type" {
+}
+
+variable "tags" {
+  type = map(string)
+}
+
